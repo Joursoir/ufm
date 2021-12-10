e20d80d2d1b910ea8e01b30dd2395e245a374262
Joursoir <chat@joursoir.net>
2021-12-10 21:25:53 +0300

    actions: add the missed header

--- Library/UefiShellUfmCommandLib/actions.c
+++ Library/UefiShellUfmCommandLib/actions.c
@@ -1,6 +1,7 @@
 #include <Library/UefiLib.h>
 #include <Library/DebugLib.h>
 #include <Library/ShellLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #include "dir.h"
